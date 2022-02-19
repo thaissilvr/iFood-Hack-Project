7b3703cc51c9e519be787c2e84f15724f01fd3ec
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-02-18 21:50:41 -0300

    Criação do Header

---
+++ my-react-app/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header () {
+    return (
+        <header className='headerMain'>
+            <div>Este é o header!</div>
+
+        </header>
+    )
+}
