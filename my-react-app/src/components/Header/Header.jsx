00136c961e7ebe6d398ea6cc93b6dbbe18060e43
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-02-19 17:36:23 -0300

    Estruturação do header + logo

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons'
 import './Header.css'
+import logo from './ifood-43.png'
 
 export default function Header () {
     return (
         <header className='headerMain'>
-            <div>Este é o header!</div>
+            <ul>
+                <img src={logo} width='50px' height='30px'/>
+                <li className='nav'>Restaurante</li>
+                <li className='nav'>Mercado</li>
+                <li className='nav'>Bebidas</li>
+                <li className='nav'>Farmácia</li>
+                <li className='nav'>Express</li>
+                <li className='nav'>Pets</li>
+
+
+                <div>
+                <input typeof='text' className='navSearch' placeholder='Busque aqui'/>
+                <FontAwesomeIcon icon={faSearch} className='nav' />
+                </div>
+
+                <FontAwesomeIcon icon={faUser} className='nav' />
+            </ul>
+
+     
+           
 
         </header>
     )
