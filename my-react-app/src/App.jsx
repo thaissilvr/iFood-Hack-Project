9bc46ab4d547937e50c42d48dfbf2bc75121bf54
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-02-18 22:13:56 -0300

    Adição do Header e Footer no projeto.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import React from 'react';
 import './App.css'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
   return (
     <div className="App">
-     <h1>Olá!</h1>
+     <Header/>
+     <Footer/>
     </div>
   )
 }
